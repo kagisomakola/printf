998c1c87f47a6cec4c7310a4185b95a813457334
kagiso <kagisomakola01@gmaul.com>
2023-11-13 04:45:35 -0800

    printf

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int printf_char(va_list val)
+int _putchar(char c);
+int _printf(const char *format, ...);
+int printf_string(va_list val);
+int _strlen(char *str);
+int _strlenc(const char *str);
+int print_37(void);
+
+#endif
